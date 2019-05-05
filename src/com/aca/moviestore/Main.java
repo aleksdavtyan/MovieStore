a4a0f090e616450bfe48750efc75533d2e238d43
aleksdavtyan <davtyan.it@gmail.com>
2019-05-05 23:57:09 +0400

    Movie Store.

---
+++ src/com/aca/moviestore/Main.java
@@ -0,0 +1,7 @@
+package com.aca.moviestore;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/aca/moviestore/ui/CommandLineUserInterface.java
@@ -0,0 +1,29 @@
+package com.aca.moviestore.ui;
+
+import java.util.Scanner;
+
+public class CommandLineUserInterface extends UserInterface {
+
+    @Override
+    public boolean output(String message) {
+        System.out.println(message);
+        return true;
+    }
+
+    @Override
+    public String readStr() {
+        //get Car Body Type
+        Scanner strScanner = new Scanner(System.in);
+        String carPartsNumbers;
+        carPartsNumbers = strScanner.nextLine();
+        return carPartsNumbers;
+    }
+
+    @Override
+    public int readInt() {
+        Scanner intScanner = new Scanner(System.in);
+        int carPartNumber;
+        carPartNumber = intScanner.nextInt();
+        return carPartNumber;
+    }
+}
